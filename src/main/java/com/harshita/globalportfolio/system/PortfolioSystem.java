29b934cd00967b6dfbacf8f8e99c917faef4b4f0
Hashdub9 <harshitadub12@gmail.com>
2026-02-10 15:34:51 +0530

    Update PortfolioSystem.java

--- src/main/java/com/harshita/globalportfolio/system/PortfolioSystem.java
+++ src/main/java/com/harshita/globalportfolio/system/PortfolioSystem.java
@@ -16,7 +16,7 @@ public class PortfolioSystem {
         this.md = md;
     }
 
-    // UPDATED: now takes homeCurrency
+    //now takes homeCurrency
     public User addUser(String name, Currency homeCurrency) {
         if (users.containsKey(name)) {
             throw new IllegalArgumentException("User already exists: " + name);
